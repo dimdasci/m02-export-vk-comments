92271d24e1c19e131d335255dc26250bbbd1a2b1
dimdasci <dimds@fastmail.com>
2022-09-30 13:27:53 +0400

    blueprint of business logic

--- src/get_comments.py
+++ src/get_comments.py
@@ -1,3 +1,4 @@
+from turtle import pos
 import vk_api
 import json
 import click
@@ -15,6 +16,30 @@ def init(id: str, password: str) -> vk_api.VkApi:
     vk_session.auth()
 
     return vk_session.get_api()
+
+
+def export_posts(vk: vk_api.VkApi, club_id: int, depth: int, filter: str = "") -> tuple:
+    """Exports posts of club_id for depth days from now.
+
+    If a filter given exports only posts that contain filter
+
+    Return list of posts and list of tuples post_id, comments_number as the queue
+    """
+    print(f"Exporting posts for {club_id} club {depth} day(s) from now.")
+
+    posts, queue = [[5, "hello"], [6, "word"]], [(5, 2), (6, 3)]
+    return posts, queue
+
+
+def export_comments(vk: vk_api.VkApi, club_id: int, post_id: int, number: int) -> list:
+    """Exports number of comments to post_id of club_id club
+
+    Returns list of comments
+    """
+    print(f"Export {number} comments for {post_id} post")
+
+    comments = []
+    return comments
 
 
 @click.command()
@@ -38,6 +63,15 @@ def get_comments(club_id: int, depth: int, filter: str) -> None:
     TIMEZONE = timezone(os.getenv("TIMEZONE"))
 
     vk = init(VK_ID, VK_PASSWORD)
+
+    posts, queue = export_posts(vk, club_id=club_id, depth=depth)
+
+    comments = []
+    for item in queue:
+        comments = comments + export_comments(
+            vk, club_id=club_id, post_id=item[0], number=item[1]
+        )
+
     print(club_id, depth, filter)
     print(f"id {VK_ID}, passw {VK_PASSWORD}, timezone {TIMEZONE}, {vk}")
 
