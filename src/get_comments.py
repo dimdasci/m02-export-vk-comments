a82d4bf5f01e4a60d6233140552dc1092719a253
dimdasci <dimds@fastmail.com>
2022-10-07 21:54:52 +0400

    added thread comments parsing and logging

--- src/get_comments.py
+++ src/get_comments.py
@@ -1,3 +1,4 @@
+from asyncio.log import logger
 import vk_api
 import click
 import datetime
@@ -6,6 +7,7 @@ import pandas
 import re
 from dotenv import load_dotenv
 import os
+import logging
 
 REMOVE_LINEBREAKS = re.compile("[\n\r]+")
 
@@ -16,6 +18,35 @@ def init(id: str, password: str) -> vk_api.VkApi:
     vk_session.auth()
 
     return vk_session.get_api()
+
+
+def setup_logging(logfile: str = "log.txt", loglevel: str = "DEBUG") -> None:
+    """
+    Sets up logging handlers and a format
+
+    :param logfile:
+    :param loglevel:
+    """
+    loglevel = getattr(logging, loglevel)
+
+    logger = logging.getLogger()
+    logger.setLevel(loglevel)
+    fmt = (
+        "%(asctime)s: %(levelname)s: %(filename)s: "
+        + "%(funcName)s(): %(lineno)d: %(message)s"
+    )
+    formatter = logging.Formatter(fmt)
+
+    fh = logging.FileHandler(logfile, encoding="utf-8")
+    fh.setLevel(loglevel)
+    fh.setFormatter(formatter)
+
+    ch = logging.StreamHandler()
+    ch.setLevel(loglevel)
+    ch.setFormatter(formatter)
+
+    logger.addHandler(fh)
+    logger.addHandler(ch)
 
 
 def export_posts(
@@ -34,37 +65,41 @@ def export_posts(
     export_term_start_time = datetime.datetime.fromtimestamp(now_time, tz=tz)
     export_term_end_time = datetime.datetime.fromtimestamp(post_time_limit, tz=tz)
 
-    print(
-        f"Exporting posts for {club_id} club since {export_term_start_time} "
-        + f"till {export_term_end_time}."
+    logging.info(
+        f"Exporting time frame is {export_term_start_time} – "
+        + f"{export_term_end_time}."
     )
 
-    response = vk.wall.get(owner_id=-club_id, count=100, filter="owner")
-
     posts = {"id": [], "url": [], "datetime": [], "text": [], "comments_number": []}
-    queue = []
-    exported_posts_count = 0
-    for item in response["items"]:
-        if filter and filter not in item["text"]:
-            print(f"post {item['id']} doesn't contain '{filter}'")
-        elif item["date"] < post_time_limit:
-            print(f"All posts up to {export_term_end_time} has been exported")
-            break
-        else:
-            posts["id"].append(item["id"])
-            posts["url"].append(
-                f"https://vk.com/club{club_id}?w=wall-{club_id}_{item['id']}"
-            )
-            posts["datetime"].append(
-                datetime.datetime.fromtimestamp(item["date"], tz=tz)
-            )
-            posts["text"].append(REMOVE_LINEBREAKS.sub(" ", item["text"]))
-            posts["comments_number"].append(item["comments"]["count"])
 
-            queue.append((item["id"], item["comments"]["count"]))
-            exported_posts_count += 1
+    try:
+        response = vk.wall.get(owner_id=-club_id, count=100, filter="owner")
 
-    print(f"{exported_posts_count} posts have been exported")
+        queue = []
+        exported_posts_count = 0
+        for item in response["items"]:
+            if filter and filter not in item["text"]:
+                logging.info(f"post {item['id']} doesn't contain '{filter}'")
+            elif item["date"] < post_time_limit:
+                logging.info(f"All posts up to {export_term_end_time} has been exported")
+                break
+            else:
+                posts["id"].append(item["id"])
+                posts["url"].append(
+                    f"https://vk.com/club{club_id}?w=wall-{club_id}_{item['id']}"
+                )
+                posts["datetime"].append(
+                    datetime.datetime.fromtimestamp(item["date"], tz=tz)
+                )
+                posts["text"].append(REMOVE_LINEBREAKS.sub(" ", item["text"]))
+                posts["comments_number"].append(item["comments"]["count"])
+
+                queue.append((item["id"], item["comments"]["count"]))
+                exported_posts_count += 1
+    except Exception as e:
+        logging.error("Got an error during posts export ", e)
+
+    logging.info(f"{exported_posts_count} posts have been exported")
     return posts, queue
 
 
@@ -79,7 +114,7 @@ def export_comments(
 
     Returns list of comments
     """
-    print(f"Looking for {number} comments of {post_id} post")
+    logging.info(f"Looking for {number} comments of {post_id} post")
 
     comments = {
         "id": [],
@@ -87,30 +122,41 @@ def export_comments(
         "url": [],
         "datetime": [],
         "text": [],
-        "thread_number": [],
+        "parent_comment_id": [],
         "likes_number": [],
     }
 
-    response = vk.wall.getComments(
-        owner_id=-club_id, post_id=post_id, count=max(100, number), need_likes=1
-    )
-    print(f"Exported {len(response['items'])} items for {response['count']} comments")
-
-    for item in response["items"]:
+    def append_comment(item, parent_comment_id=0):
         comments["id"].append(item["id"])
         comments["post_id"].append(post_id)
         comments["url"].append(
-            f"https://vk.com/wall-{club_id}_{post_id}?reply={item['id']}"
+            f"https://vk.com/wall-{club_id}_{post_id}?reply={parent_comment_id if parent_comment_id else item['id']}"
         )
         comments["datetime"].append(
             datetime.datetime.fromtimestamp(item["date"], tz=tz)
         )
         comments["text"].append(REMOVE_LINEBREAKS.sub(" ", item["text"]))
         comments["likes_number"].append(item["likes"]["count"])
-        thread_number = 0
-        if "thread" in item:
-            thread_number = item["thread"]["count"]
-        comments["thread_number"].append(thread_number)
+        comments["parent_comment_id"].append(parent_comment_id)
+
+    try:
+        response = vk.wall.getComments(
+            owner_id=-club_id, post_id=post_id, count=max(100, number), need_likes=1, 
+            thread_items_count=10
+        ) 
+
+        logging.info(
+            f"Exported {len(response['items'])} items for {response['count']} comments"
+        )
+
+        for item in response["items"]:
+            append_comment(item)
+            
+            if "thread" in item and item["thread"]["count"]:
+                for thread_item in item['thread']['items']:
+                    append_comment(thread_item, parent_comment_id=item["id"])
+    except Exception as e:
+        logging.error("Got an error during comments export ", e)
 
     return comments
 
@@ -130,6 +176,12 @@ def export_comments(
 def get_comments(club_id: int, depth: int, filter: str) -> None:
     """Exports posts of a given by CLUB_ID VK club and its comments"""
 
+    setup_logging(logfile="data/log.txt", loglevel="INFO")
+    logging.info(
+        f"Get posts and comments of {club_id} for {depth} days from now. "
+        + f"Filter is {filter if filter else 'not given'}"
+    )
+
     load_dotenv()
     VK_ID = os.getenv("VK_ID")
     VK_PASSWORD = os.getenv("VK_PASSWORD")
@@ -142,7 +194,7 @@ def get_comments(club_id: int, depth: int, filter: str) -> None:
     )
 
     if len(queue) == 0:
-        print("No comments to export")
+        logging.info("No comments to export")
         return
 
     posts_df = pandas.DataFrame(posts)
@@ -164,7 +216,7 @@ def get_comments(club_id: int, depth: int, filter: str) -> None:
     if comments_df is not None:
         comments_df.to_csv("data/comments.csv")
 
-    print(f"Exported {comments_df.shape[0]} comments total")
+    logging.info(f"Exported {comments_df.shape[0]} comments total")
 
 
 if __name__ == "__main__":
